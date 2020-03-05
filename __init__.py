14c02b1de96a2e2b651ab1db70ef7987b19a26ef
Christopher Trent <christopherJTRent@outlook.com>
2020-03-05 15:11:13 -0800

    initial commit v 0.0.1

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from TKinterManagedFrame.ManagedFrame import managedframe
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+	name='TKinterManagedFrame',
+	packages='TKinterManagedFrame',
+	version='0.0.1',
+	license='GNU GPL',
+	description='Adds a Tkinter Frame class that offers simple update functionality',
+	author='Christopher "Arkevorkhat" Trent',
+	url='',
+	install_requires=['tkinter'],
+	classifiers=[
+			  'Development :: 3 - Alpha', 
+			  'Intended Audience :: Developers',
+			  'Topic :: Software Development :: Libraries :: Tcl Extensions',
+			  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+			  'Natural Language :: English',
+			  ])
