afecd8c99ab8fe18c634819673cf2e42de91319b
Christopher Trent <christopherJTRent@outlook.com>
2020-03-05 15:28:27 -0800

    updated setup to 0.0.4

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
 	name='TKinterManagedFrame',
 	packages=['TKinterManagedFrame'],
-	version='0.0.4',
+	version='0.0.5',
 	license='GNU GPL',
 	description='Adds a Tkinter Frame class that offers simple update functionality',
 	author='Christopher "Arkevorkhat" Trent',
 	url='',
 	install_requires=['tkinter','wheel'],
 	classifiers=[
-			  'Development :: 3 - Alpha', 
+			  'Development Status :: 3 - Alpha', 
 			  'Intended Audience :: Developers',
 			  'Topic :: Software Development :: Libraries :: Tcl Extensions',
 			  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
